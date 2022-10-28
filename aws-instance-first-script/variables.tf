54e3f3ab2ccbada48ff5f1c116dc1f6f28417d4f
HemanthY15 <116891452+HemanthY15@users.noreply.github.com>
2022-10-28 18:22:07 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -7,7 +7,7 @@ variable "ami_id" {
 
   default = {
     us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
+    eu-west-2    = "ami-132b3c7efe6sdfdsfdd"
     eu-central-1 = "ami-9787h5h6nsn"
   }
 }
